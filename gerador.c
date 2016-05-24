47a9330e5bedc364c748ac0b1860a3623d7908fa
António Melo <happyd3nt@gmail.com>
2016-05-24 23:24:14 +0100

    B

--- gerador.c
+++ gerador.c
@@ -1,13 +1,18 @@
 #include "parque.h"
+#include <math.h>
+#include <unistd.h>
 
 //Global variables
 int id =0;
 int fd_g_log;
+clock_t t_inicial;
+char buffer [100];
 pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
 
 //Funcion declarations
 void* func_vehicle(void* arg);
 clock_t generate_vehicle (clock_t u_clock,clock_t i_ticks);
+void writetolog(Vehicle *v);
 
 //Main
 int main(int argc, char *argv[]) {
@@ -23,7 +28,7 @@ int main(int argc, char *argv[]) {
   fclose(gfile);
   // Open and write name of the colums
   fd_g_log = open("gerador.log",O_WRONLY,O_CREAT, 0600 );
-  char name_colums[] = "t(ticks) ; id_viat ; destin ; t_estacion ; t_vida ; obser\n";
+  char name_colums[] = "t(ticks) ; id_viat ; destin ; t_estacion ; t_vida ; observ\n";
   write(fd_g_log,&name_colums,strlen(name_colums));
 
   // Period of time in seconds to the working generator
@@ -33,23 +38,25 @@ int main(int argc, char *argv[]) {
   //Random seed generator;
   srand(time(NULL));
   //Inicial Ticks
-  clock_t t_inicial = clock();
+  t_inicial = clock();
   //Inicial Diference
   clock_t  t = clock()-t_inicial ;
+
 
   //Cicle with t_geration condicion
   while(t <= t_generation*CLOCKS_PER_SEC){
     //Generate Vehicle
     clock_t wait_time = generate_vehicle(u_clock,clock());
 
-    //Sleep wait_time seconds(WRONG CHANGE)
-    sleep(wait_time/CLOCKS_PER_SEC);
+    //Sleep wait_time seconds
+    usleep((wait_time/CLOCKS_PER_SEC)*pow(10,6));
 
     //New Diference
     t = clock()-t_inicial;
   }
 
-  pthread_exit(NULL);
+  //pthread_exit(NULL);
+  return 0;
 }
 
 clock_t generate_vehicle (clock_t u_clock,clock_t i_ticks){
@@ -59,6 +66,7 @@ clock_t generate_vehicle (clock_t u_clock,clock_t i_ticks){
 
   //ID
   v->id = id++;
+  //printf("%d   ",v->id);
 
   //Random controller entrance
   int controller = rand() % 4;
@@ -79,12 +87,14 @@ clock_t generate_vehicle (clock_t u_clock,clock_t i_ticks){
     default:
       break;
   }
+  //printf("%s   ",&v->dest);
   //Inicial Tick
   v->ini_tick = i_ticks;
+  //printf("%d   ",v->ini_tick );
 
   //Time of parking
   v->t_parking= (rand() % 10 +1)* u_clock;
-
+  //printf("%d   ",v->t_parking );
   //Next vehicle time
   clock_t next_v = rand () % 10+1;
 
@@ -100,6 +110,7 @@ clock_t generate_vehicle (clock_t u_clock,clock_t i_ticks){
 
   //Vehicle FIFO name
   sprintf(v->fifo_name,"%s%d","fifo",v->id);
+  //printf("%s   \n",&v->fifo_name);
 
   //Call thread
   if(pthread_create(&tid,NULL,func_vehicle,v) != 0){
@@ -107,32 +118,49 @@ clock_t generate_vehicle (clock_t u_clock,clock_t i_ticks){
   }
 
   //Return time in ticks that generator needs to wait
+  //free(v);
   return next_v;
 }
 
 
 //Function that the thread with tid executes when is created
 void* func_vehicle(void* arg){
-  Vehicle v= *(Vehicle*) arg;
+  Vehicle * v= (Vehicle*) arg;
+  int fd_read;
 
   //Creat FIFO
-  mkfifo(v.fifo_name,FIFO_MODE);
+  mkfifo(v->fifo_name,FIFO_MODE);
 
   //Open controller FIFO
   int fifoc;
-  if(strcmp(v.dest, "N") == 0){
+  if(strcmp(v->dest, "N") == 0){
     fifoc = open("fifoN",O_WRONLY);
-  }else if(strcmp(v.dest, "S") == 0){
+  }else if(strcmp(v->dest, "S") == 0){
     fifoc = open("fifoS",O_WRONLY);
-  }else if(strcmp(v.dest, "E") == 0){
+  }else if(strcmp(v->dest, "E") == 0){
     fifoc = open("fifoE",O_WRONLY);
   }else{
     fifoc = open("fifoO",O_WRONLY);
   }
 
-  writeVehicle(fifoc,&v);
-  //while()
-  pthread_exit(0);
+  //Write to VehicleControllerFIFO
+  writeVehicle(fifoc,v);
+
+  //Read information from controller
+  fd_read = open(v->fifo_name,O_RDONLY);
+  read(fd_read,&v->state,sizeof(v->state));
+
+  //Lock thread
+  pthread_mutex_lock(&mut);
+  //Write to log file
+  //writetolog();
+  //Unlock Thread
+  pthread_mutex_unlock(&mut);
+
+  unlink(v->fifo_name);
+  free(v);
+
+  return NULL;
 }
 
 
@@ -178,4 +206,19 @@ int readVehicle(int fd, Vehicle *v) {
 	v->t_parking = atoi(t);
 
 	return n_bytes_read;
+}
+
+void writetolog(Vehicle *v){
+
+  if(strcmp(v->state,"saida") == 0){
+    sprintf(buffer,"%-8d ;%7d;   %s  ;%10d ; %6d; %s\n",(clock()-t_inicial),v->id,v->dest,v->t_parking,(clock() - v->ini_tick),v->state);
+  }else{
+    sprintf(buffer,"%-8d ;%7d;   %s  ;%10d ;       ? ; %s\n",(clock()-t_inicial),v->id,v->dest,v->t_parking,v->state);
+  }
+
+  int ok = write(fd_g_log,buffer,strlen(buffer));
+  if(ok == 0){
+    perror("Error writing to gerator log file!!");
+  }
+  strcpy(buffer,"");
 }
